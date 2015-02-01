a88c4d05ec4a283fcf1bf2d982c4e8b28876bd14
Dinesh Thirumurthy <dinesh.thirumurthy@gmail.com>
2015-02-01 12:29:23 +0530

    id-nirmal.sh

---
+++ id-nirmal.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+git config user.name 'Nirmal Ramasubramanian'
+git config --global user.name 'Nirmal Ramasubramanian'
+git config user.email 'nirmal106110058@gmail.com'
+git config --global user.email 'nirmal106110058@gmail.com'
