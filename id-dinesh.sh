398cd99013d5020e627ebae4e0e010357cac885c
Dinesh Thirumurthy <dinesh.thirumurthy@gmail.com>
2015-02-11 05:08:11 +0530

    identity changer

---
+++ id-dinesh.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+git config user.name 'Dinesh Thirumurthy'
+git config --global user.name 'Dinesh Thirumurthy'
+git config user.email 'dinesh.thirumurthy@gmail.com'
+git config --global user.email 'dinesh.thirumurthy@gmail.com'
