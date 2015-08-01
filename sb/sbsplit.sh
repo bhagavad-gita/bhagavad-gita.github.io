0bd78c97982bab1b91de1f06a79a55ec7cc4f574
Dinesh Thirumurthy <dinesh.thirumurthy@gmail.com>
2015-08-01 19:53:41 +0530

    sbsh

--- sb/sbsplit.sh
+++ sb/sbsplit.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 # for c in 01 02 03 04 05 06 07 08 09 10
-for c in 01 02 03 04 05
+for c in 01 02 03 04 05 06 07 08 09 10
 do
 	# CMD="./sbsplit.py raw/SB-Canto-$c-Raw.txt"
 	CMD="./sbsplit.py $c"
