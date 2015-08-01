5ec60a607cefb5093a119a582a541bbbf91166b2
Dinesh Thirumurthy <dinesh.thirumurthy@gmail.com>
2015-08-01 18:50:08 +0530

    sb

--- sb/sbsplit.py
+++ sb/sbsplit.py
@@ -1,8 +1,10 @@
 #!/C/Python34/python.exe
 
+import sys
 import fileinput
 
 debug = False
+debug2 = False
 header = ""
 for line in open("../sloka-begin-html"):
 	header += line
@@ -17,69 +19,95 @@ for line in open("../sloka-end-html"):
 
 first = True
 id = ""
-for line in fileinput.input():
+tcanto = sys.argv[1]
+canto = int(tcanto)
+rawfilename = "SB-Canto-" + tcanto + "-Raw.txt"
+rawpathname = "raw/" + rawfilename
+raw = open(rawpathname, "r")
+print("Canto", canto)
+chapter = "1"
+for line in raw:
 	line = line.strip()	
+	if debug2:
+		print("LINE[", line, "]")
 	if line == "":
+		if debug:
+			print("SKIP")
 		continue
-	if "TRANSLATION" in line:
+	if "SKIP TRANSLATION" in line:
+		if debug:
+			print("SKIP")
 		continue
-	if "TEXT" in line:
+	if line == "TEXT":
+		if debug:
+			print("SKIP")
+		continue
+	if line == "TEXT 1":
+		if debug:
+			print("NEW CHAPTER BEGINS")
+		chapter = str(1 + int(chapter))
+	if "TEXT " in line:
 		if id == "":
 			beginhtml = True
 		if id != "":
 			print("</p>", file=of)
-			print("</p>", file=of0)
+			# print("</p>", file=of0)
 			print("", file=of)
-			print("", file=of0)
+			# print("", file=of0)
 			print(footer, file=of)
 			first = False
 			of.close()
 		ids = line.split()
-		# print(ids[1])
+		if debug:
+			print(ids[1])
 		line = ids[1]
 		id = line
 		if debug:
 			print("DEBUG: id to split is", id)
-		(chapter, verse) = id.split('.')
+		verse = str(id)
 		if debug:
+			print("canto", canto)
 			print("chapter", chapter)
 			print("verse", verse)
-		ofilename = id + ".html"
-		ofilename0 = chapter + ".html"
-		of = open(ofilename, "w")
+		summary = str(canto) + "." + chapter + "." + verse
+		ofilename = summary + ".html"
+		opathname = str(canto) + "/" + ofilename
+		# ofilename0 = chapter + ".html"
+		of = open(opathname, "w")
 		if debug:
-			print(ofilename)
-			print(ofilename0)
+			print(opathname)
+			# print(opathname)
 		print(header, file=of)
-		if beginhtml:
-			of0 = open(ofilename0, "w")
-			print(header, file=of0)
-			print("<title>Bhagavad-gita "+chapter+"</title>", file=of0)
-		print("<title>Bhagavad-gita "+id+"</title>", file=of)
+		# if beginhtml:
+			# of0 = open(ofilename0, "w")
+			# print(header, file=of0)
+			# print("<title>Bhagavad-gita "+chapter+"</title>", file=of0)
+		print("<title>Srimadh-bhagavatam "+summary+"</title>", file=of)
 		print(header2, file=of)
 		if beginhtml:
-			print(header2, file=of0)
+			# print(header2, file=of0)
 			beginhtml = False
 		print("<br>", file=of)
-		print("<br>", file=of0)
-		print("<h2><a href=\"http://bhagavad-gita.today/"+chapter+"\">"+chapter+"</a>."+verse+"</h2>", file=of)
-		print("<h2><a href=\"http://bhagavad-gita.today/"+chapter+"\">"+chapter+"</a>."+verse+"</h2>", file=of0)
+		# print("<br>", file=of0)
+		# print("<h2><a href=\"http://bhagavad-gita.today/"+chapter+"\">"+chapter+"</a>."+verse+"</h2>", file=of)
+		# print("<h2><a href=\"http://bhagavad-gita.today/"+chapter+"\">"+chapter+"</a>."+verse+"</h2>", file=of0)
 		print("", file=of)
-		print("", file=of0)
+		# print("", file=of0)
 		print("<p>", file=of)
-		print("<p>", file=of0)
+		# print("<p>", file=of0)
 
 		print("<a href=\"http://bhagavad-gita.today/"+id+"\"><b>"+id+"</b></a>", file=of)
-		print("<a href=\"http://bhagavad-gita.today/"+id+"\"><b>"+id+"</b></a>", file=of0)
+		# print("<a href=\"http://bhagavad-gita.today/"+id+"\"><b>"+id+"</b></a>", file=of0)
 		print("", file=of)
-		print("", file=of0)
+		# print("", file=of0)
 		continue
 	if id != "":
-		print(chapter+"."+verse, line)
+		if debug2:
+			print(str(canto)+"."+chapter+"."+verse, line)
 		print(line, file=of)
-		print(line, file=of0)
+		# print(line, file=of0)
 
 fileinput.close()
 
-print(footer, file=of0)
-of0.close()
+# print(footer, file=of0)
+# of0.close()
--- sb/sbsplit.sh
+++ sb/sbsplit.sh
@@ -1,8 +1,9 @@
 #!/bin/sh
 # for c in 01 02 03 04 05 06 07 08 09 10
-for c in 05
+for c in 01 02 03 04 05
 do
-	CMD="./sbsplit.py C$c.txt"
+	# CMD="./sbsplit.py raw/SB-Canto-$c-Raw.txt"
+	CMD="./sbsplit.py $c"
 	echo $CMD
-	# $CMD
+	$CMD
 done
